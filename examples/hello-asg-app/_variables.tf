acd7988b4914c317a963368462a6b2db0959e2b4
rafaelmarques7 <rafaelmarques76076@gmail.com>
2019-01-15 11:58:56 +0000

    improved documentation; added example

---
+++ examples/hello-asg-app/_variables.tf
@@ -0,0 +1,9 @@
+variable "aws-region" {
+  description = "The AWS region"
+  type        = "string"
+}
+
+variable "aws-profile" {
+  description = "The name of the AWS shared credentials account."
+  type        = "string"
+}
