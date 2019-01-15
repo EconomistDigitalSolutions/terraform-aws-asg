3acc9cc57fad29570a87f0e7cfc3a59eda5eb8e0
rafaelmarques7 <rafaelmarques76076@gmail.com>
2019-01-15 11:26:37 +0000

    improved documentation; added example

---
+++ examples/hello-node/deploy-hello-node.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# update packages
+sudo yum -y update
+
+# download and start docker
+sudo yum -y install docker
+
+#sudo systemctl start docker
+sudo service docker start 
+
+# pull docker app
+sudo docker pull rafaelmarques7/hello-node:latest
+
+# run docker app
+sudo docker run -d -p 80:80 rafaelmarques7/hello-node:latest
