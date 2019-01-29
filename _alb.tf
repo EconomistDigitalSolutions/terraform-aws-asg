55d042df57b9b6a8733d6edf83ef4b201c59b070
Rafael Marques <rafaelmarques76076@gmail.com>
2019-01-29 15:02:21 +0000

    Add health check for target instances
    Add health check for target instances

--- _alb.tf
+++ _alb.tf
@@ -14,6 +14,14 @@ resource "aws_lb_target_group" "lb_target" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = "${aws_vpc.vpc.id}"
+
+  health_check = {
+    interval = 6
+    timeout  = 5
+    path     = "${var.health-check-path}"
+    port     = "${var.health-check-port}"
+    matcher  = "200-299"
+  }
 }
 
 resource "aws_lb_listener" "lb_listener" {
--- examples/engagement-app/main.tf
+++ examples/engagement-app/main.tf
@@ -1,11 +1,11 @@
 module "asg" {
-  source  = "../../"
+  source = "../../"
 
   aws-profile                  = "ds-web-products-staging"
-  aws-region                   = "eu-west-3" 
-  instance-ami                 = "ami-0dd7e7ed60da8fb83"  
-  user-data-script             = "./user-data.sh" 
-  asg-min-size                 = "2"                      
+  aws-region                   = "eu-west-3"
+  instance-ami                 = "ami-0dd7e7ed60da8fb83"
+  user-data-script             = "./user-data.sh"
+  asg-min-size                 = "2"
   asg-max-size                 = "4"
   asg-def-size                 = "2"
   alb-name                     = "rafa-ian-alb"
@@ -15,5 +15,5 @@ module "asg" {
   launch-config-name           = "rafa-ian-lc"
   instance-associate-public-ip = "true"
   iam-role-name                = "engage-ECR-read"
-  ssh-allowed-ips              = ["62.255.97.196/32", "62.255.97.197/32"]
+  ssh-allowed-ips              = ["62.255.97.196/32", "62.6.58.84/32"]
 }
