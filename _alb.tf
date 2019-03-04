48c7dbebb13fa97460d9d903f7073e2c0058601e
Lizzie Hard <elizabethhard@economist.com>
2019-03-04 12:04:49 +0000

    Add SSL certificate

--- _alb.tf
+++ _alb.tf
@@ -39,4 +39,16 @@ resource "aws_lb_listener" "lb_listener" {
     type             = "forward"
     target_group_arn = "${aws_lb_target_group.lb_target.arn}"
   }
+
+  load_balancer_arn = "${aws_lb.alb.arn}"
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = "arn:aws:acm:eu-west-2:065882805973:certificate/ee9dc01b-3373-46df-aa07-9f8574c036af"
+
+  default_action {
+    type             = "forward"
+    alb_target_group_arn = "${aws_lb_target_group.lb_target.arn}"
+  }
+}
 }
