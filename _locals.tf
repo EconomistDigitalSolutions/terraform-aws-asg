5a0c465c5c1805d343c3fc0bb2601ec12d09d251
rafael <rafaelmarques76076@gmail.com>
2019-06-03 09:26:01 +0100

    Cloudfront using a single default cache

--- _locals.tf
+++ _locals.tf
@@ -1,4 +1,8 @@
 locals {
+  cdn_hostnames_aliases = "${split(",", var.environment == "production" ? join(",",var.hostnames_prod) : join(",", var.hostnames_staging))}"
+}
+
+locals {
   common_tags = {
     Product          = "${var.product}"
     Project          = "${var.project}"
@@ -7,6 +11,16 @@ locals {
     Environment      = "${var.environment}"
     LastUpdate       = "${timestamp()}"
     GitHash          = "${var.git-hash}"
+  }
+}
+
+locals {
+  common_tags_cdn = {
+    Product          = "${var.product}"
+    Project          = "${var.project}"
+    EmergencyContact = "${var.emergency-contact}"
+    Owner            = "${var.owner}"
+    Environment      = "${var.environment}"
   }
 }
 
