e30cf35af8d1299b9e71487d8d4c2154a02c8b85
rafael <rafaelmarques76076@gmail.com>
2019-06-05 09:58:49 +0100

    split route53 for cloudfront and load-balancer

--- _route53.tf
+++ _route53.tf
@@ -11,31 +11,47 @@ data "aws_acm_certificate" "example" {
   domain   = "${var.domain-name}"
 }
 
+// Route53 for Cloudfront
 resource "aws_route53_record" "www" {
-  count = "${var.domain-name != "" ? 1 : 0}"
+  count = "${var.domain-name != "" && var.use_cloudfront != "false" ? 1 : 0}"
 
   zone_id = "${data.aws_route53_zone.primary.zone_id}"
   name    = "${var.domain-name}"
   type    = "A"
 
   alias = {
-    name                   = "${var.use_cloudfront != "false" ? aws_cloudfront_distribution.cdn.domain_name : aws_lb.alb.dns_name}"
-    zone_id                = "${var.use_cloudfront != "false" ? aws_cloudfront_distribution.cdn.hosted_zone_id : aws_lb.alb.zone_id}"
+    name                   = "${aws_cloudfront_distribution.cdn.domain_name}"
+    zone_id                = "${aws_cloudfront_distribution.cdn.hosted_zone_id}"
+    evaluate_target_health = true
+  }
+}
+
+// Route53 for load balancer
+resource "aws_route53_record" "www" {
+  count = "${var.domain-name != "" && var.use_cloudfront == "false" ? 1 : 0}"
+
+  zone_id = "${data.aws_route53_zone.primary.zone_id}"
+  name    = "${var.domain-name}"
+  type    = "A"
+
+  alias = {
+    name                   = "${aws_lb.alb.dns_name}"
+    zone_id                = "${aws_lb.alb.zone_id}"
     evaluate_target_health = true
   }
 }
 
 # configure sub-domain
-resource "aws_route53_record" "sub" {
-  count = "${var.sub-domain-name != "" ? 1 : 0}"
+# resource "aws_route53_record" "sub" {
+#   count = "${var.sub-domain-name != "" ? 1 : 0}"
 
-  zone_id = "${data.aws_route53_zone.primary.zone_id}"
-  name    = "${var.sub-domain-name}"
-  type    = "A"
+#   zone_id = "${data.aws_route53_zone.primary.zone_id}"
+#   name    = "${var.sub-domain-name}"
+#   type    = "A"
 
-  alias = {
-    name                   = "${var.use_cloudfront != "false" ? aws_cloudfront_distribution.cdn.domain_name : aws_lb.alb.dns_name}"
-    zone_id                = "${var.use_cloudfront != "false" ? aws_cloudfront_distribution.cdn.hosted_zone_id : aws_lb.alb.zone_id}"
-    evaluate_target_health = true
-  }
-}
+#   alias = {
+#     name                   = "${var.use_cloudfront != "false" ? aws_cloudfront_distribution.cdn.domain_name : aws_lb.alb.dns_name}"
+#     zone_id                = "${var.use_cloudfront != "false" ? aws_cloudfront_distribution.cdn.hosted_zone_id : aws_lb.alb.zone_id}"
+#     evaluate_target_health = true
+#   }
+# }
