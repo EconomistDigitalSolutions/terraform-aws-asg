153e9783cd511850a65350c11c41143f2f78dc14
Mark Wallsgrove <markwallsgrove@users.noreply.github.com>
2020-05-12 14:38:24 +0100

    add a internal dns entry
    This will be used to divide the traffic from cloudfront to multiple
    environments by using weighted DNS on the internal DNS entry

--- _route53.tf
+++ _route53.tf
@@ -29,21 +29,38 @@ data "aws_acm_certificate" "example" {
 resource "aws_route53_record" "www" {
   count = "${var.domain-name != "" ? 1 : 0}"
 
-  depends_on = ["aws_lb.alb", "aws_cloudfront_distribution.cdn"]  
+  depends_on = ["aws_lb.alb", "aws_cloudfront_distribution.cdn"]
 
   zone_id = "${data.aws_route53_zone.primary.zone_id}"
   name    = "${var.domain-name}"
   type    = "A"
 
   alias = {
-    // we use concat because 'count' makes the response of the resource a list. 
+    // we use concat because 'count' makes the response of the resource a list.
     // link to similar issue: https://stackoverflow.com/questions/45654774/terraform-conditional-resource
-    name                   = "${var.use_cloudfront == "true" ? element(concat(aws_cloudfront_distribution.cdn.*.domain_name, list("")), 0) : aws_lb.alb.dns_name}"    
+    name                   = "${var.use_cloudfront == "true" ? element(concat(aws_cloudfront_distribution.cdn.*.domain_name, list("")), 0) : aws_lb.alb.dns_name}"
     zone_id                = "${var.use_cloudfront == "true" ? element(concat(aws_cloudfront_distribution.cdn.*.hosted_zone_id, list("")), 0) : aws_lb.alb.zone_id}"
     evaluate_target_health = true
   }
 }
 
+resource "aws_route53_record" "internal-dns" {
+  zone_id = "${data.aws_route53_zone.primary.zone_id}"
+  name    = "${var.internal-domain-name}"
+  type    = "A"
+
+  weighted_routing_policy {
+    weight = 100
+  }
+
+  set_identifier = "old_env"
+
+  alias = {
+    name = "${aws_lb.alb.dns_name}"
+    zone_id = "${aws_lb.alb.zone_id}"
+    evaluate_target_health = false
+  }
+}
 
 # configure sub-domain
 # resource "aws_route53_record" "sub" {
